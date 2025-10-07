e48a66a483c3a29d2ce09d3918bd70b972e9ca66
Dylan <dhdylanhuang@icloud.com>
2025-10-07 21:45:08 +0100

    Larger Dictionary

--- lzw.h
+++ lzw.h
@@ -2,8 +2,8 @@
 #define LZW_H
 
 #define INIT_DICT_SIZE 256
-#define CODE_BITS 13
-#define MAX_DICT_SIZE 8192
+#define CODE_BITS 14
+#define MAX_DICT_SIZE 16384
 
 #if MAX_DICT_SIZE > (1 << CODE_BITS)
 #error "MAX_DICT_SIZE must be <= 2^CODE_BITS to encode within CODE_BITS bits"
